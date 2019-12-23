9f9c090927e517c8d8d4384b650f1dc2d5e5ed70
Daniel Ortiz <dortizlira@gmail.com>
2019-12-23 13:08:52 -0500

    Remove lingui from the template (since it will be a module in atoms)

--- packages/cra-template-typescript/template/src/app/index.tsx
+++ packages/cra-template-typescript/template/src/app/index.tsx
@@ -1,33 +1,24 @@
 import * as React from "react";
-
-import {t} from "@lingui/macro";
-import {I18n, I18nProvider} from "@lingui/react";
 import Helmet from "react-helmet";
-
-import {i18n as i18nConfig} from "configs/i18n";
 
 export const App = () => {
   return (
-    <I18nProvider i18n={i18nConfig} language="en">
-        <Helmet
-          titleTemplate="Slang — %s"
-        >
-        <I18n>
-          {({i18n}) => (
-            <React.Fragment>
-              <title>{i18n._(t`Go beyond basic English`)}</title>
-              <meta
-                name="description"
-                content={i18n._(t`
-                  This is the largest platform for learning professional and specialized English.
-                  We help people, companies, and universities go beyond basic English.
-                `)}
-              />
-            </React.Fragment>
-          )}
-        </I18n>
-        </Helmet>
-        <h1> WELCOME TO THE LATEST SLANG PROJECT!! </h1>
-    </I18nProvider>
+    <React.Fragment>
+      <Helmet
+        titleTemplate="Slang — %s"
+      >
+        <React.Fragment>
+          <title>Go beyond basic English</title>
+          <meta
+            name="description"
+            content={`
+              This is the largest platform for learning professional and specialized English.
+              We help people, companies, and universities go beyond basic English.
+            `}
+          />
+        </React.Fragment>
+      </Helmet>
+      <h1> WELCOME TO THE LATEST SLANG PROJECT!! </h1>
+    </React.Fragment>
   );
 };
--- packages/cra-template-typescript/template/src/configs/i18n/index.ts
+++
@@ -1,9 +0,0 @@
-import {setupI18n} from "@lingui/core";
-
-export const i18n = setupI18n({
-  catalogs: {
-    en: require("locales/en/messages.po"),
-    es: require("locales/es/messages.po"),
-    pt: require("locales/pt/messages.po"),
-  },
-});
