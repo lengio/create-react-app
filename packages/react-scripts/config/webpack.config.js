13cf4f1afe308c7059123ef335efaebf58e6825d
Daniel Ortiz <dortizlira@gmail.com>
2020-02-06 14:54:36 -0500

    Set localsConvention to camelCaseOnly in css-loader

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -510,6 +510,7 @@ module.exports = function(webpackEnv) {
               exclude: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
+                localsConvention: "camelCaseOnly",
                 sourceMap: isEnvProduction && shouldUseSourceMap,
               }),
               // Don't consider CSS imports dead code even if the
