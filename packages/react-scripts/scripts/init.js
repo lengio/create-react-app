088bdb3a736a869425add56cf3d110fd84a9983a
Daniel Ortiz <dortizlira@gmail.com>
2020-02-13 09:56:08 -0500

    Move start-production-server and add prettier scripts

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -129,7 +129,6 @@ module.exports = function(
       build: 'react-scripts build',
       test: 'react-scripts test',
       eject: 'react-scripts eject',
-      'start-production-server': 'node src/server',
     },
     templateScripts
   );
