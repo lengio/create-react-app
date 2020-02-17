5645d94bd67e6cca2bab1ebe5a5ae91f5d30c5d7
Daniel Ortiz <dortizlira@gmail.com>
2020-02-17 09:41:20 -0500

    Add actual pre-commit file in ensurePrettierGitHook

--- packages/react-scripts/scripts/utils/ensurePrettierGitHook.js
+++ packages/react-scripts/scripts/utils/ensurePrettierGitHook.js
@@ -1,28 +1,36 @@
 'use strict';
 
 const fs = require('fs-extra');
-const chalk = require('react-dev-utils/chalk');
 const path = require('path');
+const os = require('os');
+const chalk = require('react-dev-utils/chalk');
 const paths = require('../../config/paths');
 const isInGitRepository = require('./isInGitRepository');
 
 const GIT_HOOKS_PATH = path.join('.git', 'hooks');
 
 module.exports = () => {
-  const prettierHookDir = path.join(paths.appPath, GIT_HOOKS_PATH);
-  const prettierHookFileDir = path.join(prettierHookDir, 'prettier.sh');
-
-  // exit if not in git repository
+  // Exit if not in git repository
   if (!isInGitRepository()) {
     process.exit(0);
   }
-  // exit if the hook is already created
-  if (fs.existsSync(prettierHookDir) && fs.existsSync(prettierHookFileDir)) {
-    console.log(`Prettier hook exist in ${chalk.cyan(prettierHookFileDir)}`);
+
+  const gitHooksDir = path.join(paths.appPath, GIT_HOOKS_PATH);
+  const preCommitHookPath = path.join(gitHooksDir, 'pre-commit.sh');
+  const cmd = 'exec yarn react-scripts prettier';
+
+  // Make sure the pre-commit file exist before reading/writing
+  // from it (this will create the file if it doesn't exist)
+  fs.ensureFileSync(preCommitHookPath);
+
+  const content = fs.readFileSync(preCommitHookPath, 'utf8');
+
+  // Exit if cmd already exits in pre-commit
+  if (content.match(new RegExp(cmd))) {
+    console.log(`Prettier hook exist in ${chalk.cyan(preCommitHookPath)}`);
     process.exit(0);
   }
 
-  fs.ensureFileSync(prettierHookFileDir);
-  fs.writeFileSync(prettierHookFileDir, 'TODO: get content from husky hooks');
-  console.log(`Prettier hook added to ${chalk.cyan(prettierHookFileDir)}`);
+  fs.writeFileSync(preCommitHookPath, content + os.EOL + cmd + os.EOL);
+  console.log(`Prettier hook added to ${chalk.cyan(preCommitHookPath)}`);
 };
