297ba1c32c6bb5da82fd2becb32bb55196732d92
Daniel Ortiz <dortizlira@gmail.com>
2020-03-02 14:46:08 -0500

    Add slang-atoms to paths

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -75,6 +75,7 @@ module.exports = {
   testsSetup: resolveModule(resolveApp, 'src/setupTests'),
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
+  slangAtoms: resolveApp('node_modules/@lengio/slang-atoms'),
   publicUrlOrPath,
 };
 
@@ -100,6 +101,7 @@ module.exports = {
   testsSetup: resolveModule(resolveApp, 'src/setupTests'),
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
+  slangAtoms: resolveApp('node_modules/@lengio/slang-atoms'),
   publicUrlOrPath,
   // These properties only exist before ejecting:
   ownPath: resolveOwn('.'),
@@ -139,6 +141,7 @@ if (
     testsSetup: resolveModule(resolveOwn, `${templatePath}/src/setupTests`),
     proxySetup: resolveOwn(`${templatePath}/src/setupProxy.js`),
     appNodeModules: resolveOwn('node_modules'),
+    slangAtoms: resolveApp('node_modules/@lengio/slang-atoms'),
     publicUrlOrPath,
     // These properties only exist before ejecting:
     ownPath: resolveOwn('.'),
