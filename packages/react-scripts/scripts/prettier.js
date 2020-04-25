4bb5933f52c4fab919cb454afc1bfbeb57fadc3e
Daniel Ortiz <dortizlira@gmail.com>
2020-04-25 14:04:21 -0500

    Add --all option to prettier command

--- packages/react-scripts/scripts/prettier.js
+++ packages/react-scripts/scripts/prettier.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
@@ -25,10 +26,16 @@ const rules = [
 
 // Get staged files on git
 const gitDiffCmd = `git diff --cached --name-only --diff-filter=ACMR "*.js" "*.jsx" "*.ts" "*.tsx" "*.css"`;
-const targetFiles = execSync(gitDiffCmd)
-  .toString()
-  .split(os.EOL)
-  .join(' ');
+function getStagedFiles() {
+  return execSync(gitDiffCmd)
+    .toString()
+    .split(os.EOL)
+    .join(' ');
+};
+
+const targetFiles = process.argv.includes('--all')
+  ? '"**/*.{ts,tsx,js,jsx,css}"'
+  : getStagedFiles();
 
 // Exit if no targetFiles
 if (!targetFiles) {
@@ -38,7 +45,9 @@ if (!targetFiles) {
 // Run prettier on the staged files
 execSync(`prettier ${rules.join(' ')} --write ${targetFiles}`);
 
-// Add files back to the commit after running prettier
-execSync(`git add ${targetFiles}`);
+if (!process.argv.includes('--all')) {
+  // Add files back to the commit after running prettier
+  execSync(`git add ${getStagedFiles()}`);
+}
 
 console.log(`\n${chalk.cyan('✓')} Prettier finished\n`);
