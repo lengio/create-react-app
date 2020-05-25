5b2d53ce8e0cd29de794e371e948b2fa28538ac8
Daniel Ortiz <dortizlira@gmail.com>
2020-05-25 08:55:27 -0500

    Remove prettier rules that use default values

--- packages/react-scripts/scripts/utils/prettierRules.js
+++ packages/react-scripts/scripts/utils/prettierRules.js
@@ -2,18 +2,14 @@
 
 const API_RULES = {
   printWidth: 100,
-  tabWidth: 2,
   singleQuote: true,
   trailingComma: 'all',
   bracketSpacing: false,
-  arrowParens: 'always',
 };
 
 const CLI_RULES = [
   `--print-width ${API_RULES.printWidth}`,
-  `--tab-width ${API_RULES.tabWidth}`,
   `--trailing-comma ${API_RULES.trailingComma}`,
-  `--arrow-parens ${API_RULES.arrowParens}`,
   ...(API_RULES.singleQuote && ['--single-quote']),
   ...(!API_RULES.bracketSpacing && ['--no-bracket-spacing']),
 ];
